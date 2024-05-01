a4fae56993eb823d454f905bd18d1ee9c41733d2
nakyja <najoshz@comunidad.unam.mx>
2024-05-01 17:10:04 -0600

    entregable

--- js/request.js
+++ js/request.js
@@ -12,11 +12,45 @@ btnRemove.addEventListener("click",function(e){
 function getData(url){
     xhr.open("GET", requestUrl, true); 
     xhr.onload = function(url){
-        console.log(xhr.responseText);
+        // console.log(xhr.responseText);
         // Handle data
         array = JSON.parse(xhr.responseText);
         data.json=array;
+        array.forEach(element => {
+            tbody.append(genTr(element));
+        });
     };
     xhr.send();
 }
+function genTr(json) {
+    tr = document.createElement("tr");
+    td1 = document.createElement("td");
+    td2 = document.createElement("td");
+    td3 = document.createElement("td");
+    td4 = document.createElement("td");
+    td5 = document.createElement("td");
+
+    td1.innerText = json.id;
+    td2.innerText = json.title;
+    td3.innerText = json.price;
+    td4.innerText = json.description;
+    td5.innerText = json.category;
+    tr.append(td1,td2,td3,td4,td5);
+    return tr;
+}
+
+btnLoad= document.getElementById("btnLoad");
+btnLoad.addEventListener("click",function(e){
+    getData();
+})
+
+btnSearch = document.getElementById("btnSearch");
+input = document.getElementById("inputSearch");
+btnSearch.addEventListener("click",function(e){
+    tbody.innerHTML="";
+    filtered = data.json.filter(e=>e.title.includes(input.value));
+    filtered.forEach(function(i){
+        tbody.append( genTr(i));
+    });
+});
 getData();
