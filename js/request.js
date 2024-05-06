fb41735a749cc523db4dfd020d6b58f77d43acf8
Saulitojh <pedrosauljh@comunidad.unam.mx>
2024-05-05 23:22:33 -0600

    entregable

--- js/request.js
+++ js/request.js
@@ -1,22 +1,63 @@
 
-var data ={}
+var data = {}
 var xhr = new XMLHttpRequest();
 var requestUrl = "https://fakestoreapi.com/products";
-tbody= document.getElementById("tableBodyData");
-btnRemove=document.getElementById("btnRemove");
-btnRemove.addEventListener("click",function(e){
+var tbody = document.getElementById("tableBodyData");
+var btnRemove = document.getElementById("btnRemove");
+var btnLoadData = document.getElementById("btnLoadData");
+var searchButton = document.getElementById("btnSearch");
+var input = document.getElementById("searchInput");
+
+btnRemove.addEventListener("click", function (e) {
     tbody.innerHTML = "";
 });
 
-//consumir api
-function getData(url){
-    xhr.open("GET", requestUrl, true); 
-    xhr.onload = function(url){
+function genTr(json) {
+    var tr = document.createElement("tr");
+    var td1 = document.createElement("td");
+    var td2 = document.createElement("td");
+    var td3 = document.createElement("td");
+    var td4 = document.createElement("td");
+    var td5 = document.createElement("td");
+
+    td1.innerText = json.id;
+    td2.innerText = json.title;
+    td3.innerText = json.price;
+    td4.innerText = json.description;
+    td5.innerText = json.category;
+
+    tr.append(td1, td2, td3, td4, td5);
+    return tr;
+}
+
+function getData(url) {
+    xhr.open("GET", requestUrl, true);
+    xhr.onload = function (url) {
         console.log(xhr.responseText);
         // Handle data
-        array = JSON.parse(xhr.responseText);
-        data.json=array;
+        var array = JSON.parse(xhr.responseText);
+        data.json = array;
+
+        // Recorrer el array y agregar filas a la tabla
+        array.forEach(element => {
+            tbody.append(genTr(element));
+        });
     };
     xhr.send();
 }
+
 getData();
+
+btnLoadData.addEventListener("click", function (e) {
+    getData();
+});
+
+searchButton.addEventListener("click", function () {
+    tbody.innerHTML = ''; // Vacía la tabla
+    var filtered = data.json.filter(function (item) {
+        return item.title.includes(input.value);
+    });
+    filtered.forEach(element => {
+        tbody.append(genTr(element));
+    });
+});
